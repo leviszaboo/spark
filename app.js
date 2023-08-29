a607134c2a87b419b7d74e2c23789d8df8ea213f
Levi <leviszaboo@icloud.com>
2023-08-29 16:51:24 +0200

    setting up express and graphql

--- app.js
+++ app.js
@@ -1,12 +1,55 @@
 const express = require('express');
+const { createYoga } = require('graphql-yoga')
+const { 
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull
+} = require("graphql");
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    events: {
+      type: GraphQLNonNull(GraphQLList(GraphQLNonNull(GraphQLString))),
+      resolve: () => ["Cooking", "Cruising", "Biking"]
+    }
+  }),
 })
 
-app.listen(3000);
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "Root Mutation",
+  fields: () => ({
+    createEvent: {
+      type: GraphQLString,
+      description: "Create a new event",
+      args: {
+        name: {
+          type: GraphQLString
+        }
+      },
+      resolve: (args) => args.name
+    }
+  })
+})
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: RootMutationType
+})
+
+app.all('/graphql', createYoga({ 
+  schema: schema,
+  graphiql: true
+})); 
+
+app.listen({port: 3000});
